d0be6133955c9e1f5d034b9f0339fd341eeb2425
lsilvamatheus <lsilva.matheus@gmail.com>
2022-10-03 01:10:12 -0300

    Classes de inicialização da aplicação e testes geradas pelo Spring

---
+++ src/test/java/com/lsilva/matheus/springboot2/Springboot2EssencialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsilva.matheus.springboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2EssencialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
